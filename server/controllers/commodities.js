cb19fadafdd031f8ad8bb2533315c87501d88e05
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2016-02-25 13:37:27 +0200

    SAVEPOINT

--- server/controllers/commodities.js
+++ server/controllers/commodities.js
@@ -316,6 +316,31 @@ exports.getCommodityByID = function(req, res) {
 			callback(null, commodity);
 		}
 	}
+	function getContracts(commodity, callback) {
+		commodity.contracts = [];
+		var contract_counter = 0;
+		var contract_len = commodity.contracts_link.length;
+		if(contract_len>0) {
+			_.each(commodity.contracts_link, function (contract) {
+				request('http://rc-api-stage.elasticbeanstalk.com/api/contract/' + contract._id + '/metadata', function (err, res, body) {
+					var body = JSON.parse(body);
+					++contract_counter;
+					commodity.contracts.push({
+						_id: contract._id,
+						contract_name: body.name,
+						contract_country: body.country,
+						contract_commodity: body.resource
+					});
+					if (contract_counter == contract_len) {
+						callback(null, commodity);
+					}
+				});
+
+			});
+		} else{
+			callback(null, commodity);
+		}
+	}
 	function getProjectLocation(commodity,callback) {
 		var project_counter = 0;
 		commodity.location = [];
