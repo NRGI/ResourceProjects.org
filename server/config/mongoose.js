5b85a8b3f6987cbd9f0be86c410b6b358b503237
Chris Perry <byndcivilization@gmail.com>
2016-03-11 17:28:30 -0500

    fixed connection to compose.io deployment using host certs

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -28,44 +28,116 @@ model_load.forEach(function(model_name) {
 });
 
 module.exports 	= function(config, user, pass, env) {
-
     if (env === 'local') {
         mongoose.connect(config.db);
     } else {
-        //var ca = [fs.readFileSync(__dirname + "/servercert.crt")];
-        //var options = {
-        //    mongos: {
-        //        ssl: true,
-        //        sslValidate: true,
-        //        sslCA:ca,
-        //    }
-        //};
-        mongoose.connect('mongodb://' + user + ':' + pass + config.db);
-        //db.on('open', function (err) {
-        //    assert.equal(null, err);
-        //    db.db.listCollections().toArray(function(err, collections) {
-        //        assert.equal(null, err);
-        //        collections.forEach(function(collection) {
-        //            console.log(collection);
-        //        });
-        //        db.db.close();
-        //        process.exit(0);
-        //    })
-        //});
+        var ca = [fs.readFileSync(__dirname + "/certs/servercert.crt")];
+        var options = {
+            mongos: {
+                ssl: true,
+                sslValidate: true,
+                sslCA:ca,
+            }
+        };
+        mongoose.connect('mongodb://' + user + ':' + pass + config.db, options);
     }
+
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error...'));
     db.once('open', function callback() {
-        console.log('Resource Projects  db opened');
+        console.log('rgi db opened');
     });
 
-    //// connect to mongodb
-    //mongoose.connect(config.db);
-    //var db = mongoose.connection;
-    //db.on('error', console.error.bind(console,'connection error...'));
-    //db.once('open', function callback() {
-    //    console.log('Resource Projects db opened');
-    //});
+
+
+
+//
+//
+//    var db = mongoose.connection;
+//
+//    db.on('error', console.error.bind(console, 'connection error...'));
+//    db.once('open', function callback() {
+//        console.log('Resource Projects  db opened');
+//    });
+//
+//    // connect to mongodb
+//    mongoose.connect(config.db);
+//    var db = mongoose.connection;
+//    db.on('error', console.error.bind(console,'connection error...'));
+//    db.once('open', function callback() {
+//        console.log('Resource Projects db opened');
+//    });
+//
+//
+//
+//
+//
+//    console.log(__dirname);
+//    var ca = [fs.readFileSync(__dirname + "/certs/servercert.crt")];
+//    var options = {
+//        mongos: {
+//            ssl: true,
+//            sslValidate: true,
+//            sslCA:ca,
+//        }
+//    };
+//    console.log(__dirname);
+//    //console.log(options);
+//    mongoose.connect('mongodb://cperry:50Crat3s' + config.db, options);
+//
+//// If the connection throws an error
+//    db.on('error',function (err) {
+//        console.log('Mongoose connection error: ' + err);
+//    });
+//    db.once('open', function callback() {
+//        console.log('Resource Projects db opened');
+//    });
+//    //mongoose.connection.on('open', function callback() {
+//    //    console.log('Resource Projects  db opened');
+//    //});
+//
+//    //mongoose.connection.on('open', function (err) {
+//    //    assert.equal(null, err);
+//    //    mongoose.connection.db.listCollections().toArray(function(err, collections) {
+//    //        assert.equal(null, err);
+//    //        collections.forEach(function(collection) {
+//    //            console.log(collection);
+//    //        });
+//    //        mongoose.connection.db.close();
+//    //        process.exit(0);
+//    //    })
+//    //});
+//
+//// Let's open that connection
+////    mongoose.connect('mongodb://cperry:50Crat3s' + config.db, options);
+//
+//    //mongoose.connect('mongodb://cperry:50Crat3s' + config.db);
+//    //
+//    ////if (env === 'local') {
+//    ////    mongoose.connect(config.db);
+//    ////} else {
+//    ////    //var ca = [fs.readFileSync(__dirname + "/servercert.crt")];
+//    ////    //var options = {
+//    ////    //    mongos: {
+//    ////    //        ssl: true,
+//    ////    //        sslValidate: true,
+//    ////    //        sslCA:ca,
+//    ////    //    }
+//    ////    //};
+//    ////    mongoose.connect('mongodb://' + user + ':' + pass + config.db);
+//    ////    //db.on('open', function (err) {
+//    ////    //    assert.equal(null, err);
+//    ////    //    db.db.listCollections().toArray(function(err, collections) {
+//    ////    //        assert.equal(null, err);
+//    ////    //        collections.forEach(function(collection) {
+//    ////    //            console.log(collection);
+//    ////    //        });
+//    ////    //        db.db.close();
+//    ////    //        process.exit(0);
+//    ////    //    })
+//    ////    //});
+//    ////}
+
     userModel.createDefaultUsers();
     sourceModel.createDefaultSources();
     companyModel.createDefaultCompanies();
