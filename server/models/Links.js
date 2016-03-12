0d3a515d4f54620b48f25a025128c88f4eae59fb
Chris Perry <byndcivilization@gmail.com>
2016-03-11 22:29:28 -0500

    brought transfer links back to link model

--- server/models/Links.js
+++ server/models/Links.js
@@ -106,6 +106,7 @@ function createDefaultLinks() {
             Link.create({production:'56be54f9d7bff9921c93c988',project:'56a930f41b5482a31231ef42',source:'56747e060e8cc07115200ee3',entities:['project','production']});
             Link.create({production:'56be54f9d7bff9921c93c989',project:'56a930f41b5482a31231ef42',source:'56747e060e8cc07115200ee3',entities:['project','production']});
             Link.create({production:'56be54f9d7bff9921c93c990',project:'56a930f41b5482a31231ef42',source:'56747e060e8cc07115200ee3',entities:['project','production']});
+
             //country
 
             //companies
