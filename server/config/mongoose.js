fbfe056f7543a9a11ba7cab1cdac03d2785621d1
Chris Perry <byndcivilization@gmail.com>
2016-03-14 16:24:42 -0400

    SAVEPOINT

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -36,7 +36,7 @@ module.exports 	= function(config, user, pass, env) {
             mongos: {
                 ssl: true,
                 sslValidate: true,
-                sslCA:ca,
+                sslCA:ca
             }
         };
         mongoose.connect('mongodb://' + user + ':' + pass + config.db, options);
