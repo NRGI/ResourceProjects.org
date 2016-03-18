c415fc4c22bf3fa0a4cb43d2a3dfbe1aa3bdf768
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2016-03-18 17:00:08 +0200

    SAVEPOINT

--- public/app/projects/nrgiProjectDetailCtrl.js
+++ public/app/projects/nrgiProjectDetailCtrl.js
@@ -6,10 +6,20 @@ angular.module('app')
         nrgiAuthSrvc,
         nrgiIdentitySrvc,
         nrgiProjectsSrvc,
-        $routeParams
+        $routeParams,
+        $location
     ) {
+        var id = window.localStorage.getItem('id_project');
+        var rout = window.localStorage.getItem('routing_project');
+        var location = $location.path();
+        location = location.split('/project/')[1];
+        if(id!=undefined && rout==location && rout!=undefined){
+            var project_id=id;
+        }else{
+            $location.path('/projects');
+        }
         $scope.projectMarkers=[]; $scope.center=[];
-        nrgiProjectsSrvc.get({_id: $routeParams.id}, function (success) {
+        nrgiProjectsSrvc.get({_id: project_id}, function (success) {
             $scope.project = success;
             if($scope.project.coordinates.length>0)
             $scope.center={lat:$scope.project.coordinates[0].lat,lng:$scope.project.coordinates[0].lng,zoom: 5};
