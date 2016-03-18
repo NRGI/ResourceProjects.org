7c46da96a085ee16a0da83c7995a3258df267629
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2016-03-18 17:00:08 +0200

    SAVEPOINT

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,10 @@ var rootPath 	= path.normalize(__dirname + '/../../');
 
 module.exports 	= {
     local: {
-        baseUrl: 'http://localhost:3016',
+        baseUrl: 'http://localhost:3051',
         db: 'mongodb://localhost/rp_local',
         rootPath: rootPath,
-        port: process.env.PORT || 3016
+        port: process.env.PORT || 3051
     },
     staging : {
         baseUrl: 'http://dev.resourceprojects.org',
