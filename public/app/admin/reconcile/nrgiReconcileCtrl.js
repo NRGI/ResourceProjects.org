5bc8c83a4f8cb13a501ce8cf2b056d235008fbf9
Matt Fullerton <matt.fullerton@gmail.com>
2016-03-21 15:33:18 +0100

    Model for duplicates, sample data, stub for reconciliation interface

--- public/app/admin/reconcile/nrgiReconcileCtrl.js
+++ public/app/admin/reconcile/nrgiReconcileCtrl.js
@@ -1,5 +1,5 @@
 angular.module('app')
-    .controller('nrgiEtlCtrl', function(
+    .controller('nrgiReconcileCtrl', function(
             $scope,
             $route,
             nrgiDatasetSrvc,
--- server/models/Datasets.js
+++ server/models/Datasets.js
@@ -12,7 +12,8 @@ datasetSchema = new Schema ({
     source_url: String,
     created: { type: Date, default: Date.now },
     created_by: {type: ObjectId, ref: 'User'},
-    actions: [{type: ObjectId, ref: 'Action'}]
+    actions: [{type: ObjectId, ref: 'Action'}],
+    type: { type: String, default: 'Google Sheets'}
 });
 
 Dataset = mongoose.model('Dataset', datasetSchema);
@@ -27,6 +28,17 @@ function createDefaultDatasets() {
                 created: Date.now(),
                 created_by: '56d011292cb1766d0c308ec0',
                 actions: []
+            });
+            Dataset.create({
+                _id: '56737e170e8cc07115211ee4',
+                name: "Companies House API",
+                type: "Placeholder for CRON Job",
+                source_url: 'https://extractives.companieshouse.gov.uk/api/',
+                created: Date.now(),
+                created_by: '56d011292cb1766d0c308ec0',
+                actions: ['56737e170e4ac07115211ee4']
+            }, function(err, model) {
+            console.log(err);
             });
         }
     });
