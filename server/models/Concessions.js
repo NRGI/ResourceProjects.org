8bfa0061fc2dfa9c2dc4a5886fe881c1642b2b29
Chris Perry <byndcivilization@gmail.com>
2016-03-21 18:28:14 -0400

    SAVEPOINT

--- server/models/Concessions.js
+++ server/models/Concessions.js
@@ -37,6 +37,7 @@ concessionSchema = new Schema ({
     concession_established_source: source,
     description: htmlSettings,
     concession_country: [fact],
+    concessiom_operated_by: [fact],
     concession_status: [{
         source: source,
         string: {
