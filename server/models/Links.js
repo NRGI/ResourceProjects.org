a185b8d89ab7ca69aff7fa6d50aa44537fcfc8d1
Chris Perry <byndcivilization@gmail.com>
2016-03-21 14:38:20 -0400

    SAVEPOINT

--- server/models/Links.js
+++ server/models/Links.js
@@ -98,27 +98,7 @@ function createDefaultLinks() {
             Link.create({site:'56eb117c0007bf5b2a3e4b71',project:'56a930f41b5482a31231ef42',source:'56747e060e8cc07115200ee3',entities:['site','project']});
             Link.create({site:'56eb117c0007bf5b2a3e4b76',project:'56a930f41b5482a31231ef42',source:'56747e060e8cc07115200ee3',entities:['site','project']});
             Link.create({site:'56eb117c0007bf5b2a3e4b76',project:'56a930f41b5482a31231ef44',source:'56747e060e8cc07115200ee3',entities:['site','project']});
-
-
-            // //project-transfer
-            // 
-
-            // //company-transfer
-            // Link.create({transfer:'56be54f9d7bff9921c93c989',company:'56a13a758f224f670e6a376e',source:'56747e060e8cc07115200ee3',entities:['company','transfer']});
-            // Link.create({transfer:'56be54f9d7bfflll1c93c985',company:'56a13a758f224f670e6a376e',source:'56747e060e8cc07115200ee3',entities:['company','transfer']});
-            // //company-production
-            // Link.create({production:'56be54f9d7bff9921c93c985',company:'56a13a758f224f670e6a376e',source:'56747e060e8cc07115200ee3',entities:['company','production']});
-            // Link.create({production:'56be54f9d7bff9921c93c986',company:'56a13a758f224f670e6a376e',source:'56747e060e8cc07115200ee3',entities:['company','production']});
-            // //site-transfer
             
-            
-
-
-
-
-
-
-
             //Link.create({company_group:'',company:'',source:'',entities:['company','company_group']});
             //Link.create({company_group:'',company:'',source:'',entities:['company','company_group']});
             //Link.create({company_group:'',company:'',source:'',entities:['company','company_group']});
