ed3524308f42c0e53d4fdaa6312d3885e1182abd
Chris Perry <byndcivilization@gmail.com>
2016-03-22 11:05:03 -0400

    removed cruft from country model. g gpo

--- server/models/Countries.js
+++ server/models/Countries.js
@@ -18,7 +18,6 @@ countrySchema = new Schema({
         type: ObjectId,
         ref: 'Alias'}],
     projects:[fact],
-    country_type: [fact],
     country_commodity: [fact]
 });
 
@@ -30,29 +29,6 @@ Country = mongoose.model('Country', countrySchema);
 function createDefaultCountries() {
     Country.find({}).exec(function(err, countries) {
         if(countries.length === 0) {
-            Country.create({_id:'56a7e6c02302369318e16bb8', iso2:'BG', name:'Bulgaria',country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-                country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]});
-            Country.create({_id:'56a7e6c02302369318e16bb9', iso2:'AF', name:'Afghanistan',country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-                country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]});
-            Country.create({_id:'56a7e6c02302369318e16bba', iso2:'NG', name:'Nigeria',country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-                country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]});
-            Country.create({_id:'56a8d7d08e7079da05d6b542', iso2:'GH', name:'Ghana',country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-                country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]});
-           //Country.create({
-            //    _id:'56a7e6c02302369318e16bb8',
-            //    iso2:'BG',
-            //    name:'Bulgaria',
-            //    country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-            //    country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]
-            //});
-            //Country.create({_id:'56a7e6c02302369318e16bb9', iso2:'AF', name:'Afghanistan',country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-            //    country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]});
-            //Country.create({_id:'56a7e6c02302369318e16bba', iso2:'NG', name:'Nigeria',country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-            //    country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]});
-            //Country.create({_id:'56a8d7d08e7079da05d6b542', iso2:'GH', name:'Ghana',country_type: [{source: '56747e060e8cc07115200ee3', string: 'mining'}],
-            //    country_commodity: [{source: '56747e060e8cc07115200ee3', commodity: '56a13e9942c8bef50ec2e9e8'}]});
-
-
             Country.create({_id:'56a7e6c02302369318e16bb8',iso2:'AD',name:'Andorra',cctld:'.ad'});
             Country.create({_id:'56a7e6c02302369318e16bb9',iso2:'AE',name:'United Arab Emirates',cctld:'.ae'});
             Country.create({_id:'56a7e6c02302369318e16bba',iso2:'AF',name:'Afghanistan',cctld:'.af'});
