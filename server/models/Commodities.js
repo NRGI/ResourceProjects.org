365c33f8ced0845d944fa901f104e87ff5561b53
Chris Perry <byndcivilization@gmail.com>
2016-03-22 11:09:27 -0400

    added stock hydrocarbons commodity type to deal with rc

--- server/models/Commodities.js
+++ server/models/Commodities.js
@@ -22,18 +22,6 @@ commoditySchema = new Schema ({
     commodity_aliases: [{
         type: ObjectId,
         ref: 'Alias'}]
-    //concessions: [{
-    //    type: ObjectId,
-    //    ref: 'Link'}],
-    //contracts: [{
-    //    type: ObjectId,
-    //    ref: 'Link'}],
-    //companies: [{
-    //    type: ObjectId,
-    //    ref: 'Link'}],
-    //projects: [{
-    //    type: ObjectId,
-    //    ref: 'Link'}]
 });
 
 Commodity = mongoose.model('Commodity', commoditySchema);
@@ -41,30 +29,6 @@ Commodity = mongoose.model('Commodity', commoditySchema);
 function createDefaultCommodities() {
     Commodity.find({}).exec(function(err, commodities) {
         if(commodities.length === 0) {
-            //Commodity.create({
-            //    _id: '56a13e9942c8bef50ec2e9e8',
-            //    commodity_name: 'Aluminum',
-            //    commodity_type: 'mining',
-            //    commodity_aliases: ['56a6ac8f6c1ac5811ae27988','56a6ac8f6c1ac5811ae27989','56a6ac8f6c1ac5811ae2798a']
-            //});
-            //Commodity.create({
-            //    _id: '56a13e9942c8bef50ec2e9eb',
-            //    commodity_name: 'Gold',
-            //    commodity_type: 'mining',
-            //    commodity_aliases: ['56a6ac8f6c1ac5811ae2798d','56a6ac8f6c1ac5811ae2798e']
-            //});
-            //Commodity.create({
-            //    _id: '56a13e9942c8bef50ec2e9ee',
-            //    commodity_name: 'Hydrocarbons',
-            //    commodity_type: 'Oil and Gas',
-            //    commodity_aliases: ['56a6ac8f6c1ac5811ae2798f','56a6ac8f6c1ac5811ae27990']
-            //});
-            //Commodity.create({
-            //    _id: '56a13e9942c8bef50ec2e9f1',
-            //    commodity_name: 'Diamonds',
-            //    commodity_type: 'mining',
-            //    commodity_aliases: ['56a6ac8f6c1ac5811ae27991', '56a6ac8f6c1ac5811ae27992']
-            //});
             Commodity.create({_id: '56a13e9942c8bef50ec2e9e8',commodity_name:'Antimony',commodity_type:'mining',commodity_id:'antimony',commodity_aliases: ['56a6ac8f6c1ac5811ae27988','56a6ac8f6c1ac5811ae27989','56a6ac8f6c1ac5811ae2798a']});
             Commodity.create({_id: '56a13e9942c8bef50ec2e9eb',commodity_name:'Ferrotitanium',commodity_type:'mining',commodity_id:'ferrotitanium',commodity_aliases: ['56a6ac8f6c1ac5811ae2798d','56a6ac8f6c1ac5811ae2798e']});
             Commodity.create({_id: '56a13e9942c8bef50ec2e9ee',commodity_name:'Ferrotungsten',commodity_type:'mining',commodity_id:'ferrotungsten',commodity_aliases: ['56a6ac8f6c1ac5811ae2798f','56a6ac8f6c1ac5811ae27990']});
@@ -243,6 +207,7 @@ function createDefaultCommodities() {
             Commodity.create({commodity_name:'Zeolites',commodity_type:'mining',commodity_id:'zeolites'});
             Commodity.create({commodity_name:'Oil',commodity_type:'oil_and_gas',commodity_id:'oil'});
             Commodity.create({commodity_name:'Gas',commodity_type:'oil_and_gas',commodity_id:'gas'});
+            Commodity.create({commodity_name:'Hydrocarbons',commodity_type:'oil_and_gas',commodity_id:'hydrocarbons'});
             console.log('Commodities created...');
         } else {
             console.log(String(commodities.length), 'commodities exist...')
