6b731b28846a631f884b0b67b568a002a16b03ca
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2016-03-22 14:18:26 +0200

    add direct payment count and associated payment count to project list page

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,10 @@ var rootPath 	= path.normalize(__dirname + '/../../');
 
 module.exports 	= {
     local: {
-        baseUrl: 'http://localhost:3029',
+        baseUrl: 'http://localhost:3033',
         db: 'mongodb://localhost/rp_local',
         rootPath: rootPath,
-        port: process.env.PORT || 3029
+        port: process.env.PORT || 3033
     },
     staging : {
         baseUrl: 'http://dev.resourceprojects.org',
