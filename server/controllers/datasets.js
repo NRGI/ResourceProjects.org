c323170039b391e64441353a341d110ac4f8cbb2
Mathias Baur <mathias@smartlane.de>
2016-03-24 16:21:54 +0100

    companies house import dataset and test data added

--- server/controllers/datasets.js
+++ server/controllers/datasets.js
@@ -4,6 +4,7 @@ var Dataset 		= require('mongoose').model('Dataset'),
     async           = require('async'),
     _               = require("underscore"),
     googlesheets    = require('../dataprocessing/googlesheets.js');
+	companieshouse  = require('../dataprocessing/companieshouse.js');
 
 exports.getDatasets = function(req, res) {
     var dataset_len, limit = null, skip = 0;
@@ -125,6 +126,7 @@ exports.createAction = function(req, res, next) {
                 {$push: {"actions": amodel._id}},
                 {safe: true, upsert: false, new: true},
                 function(err, dmodel) {
+			console.log("found: " + dmodel._id);
                     if (!err && dmodel) {
                         if (req.body.name == "Import from Google Sheets") {
                             console.log("Starting import from " + dmodel.name);
@@ -145,6 +147,27 @@ exports.createAction = function(req, res, next) {
                                 );
                             });
                         }
+                        else {
+				if (req.body.name == "Import from Companies House API") {
+		                        console.log("Starting import from " + dmodel.name);
+		                        res.status(200);
+		                        res.send();
+		                        console.log("Triggered, res sent\n");
+		                        companieshouse.importData(function(status, report) {
+						console.log("process finished");
+		                            console.log("Status: " + status + "\n");
+		                            console.log("Report: " + report + "\n");
+		                            Action.findByIdAndUpdate(
+				                    amodel._id,
+				                    {finished: Date.now(), status: status, details: report},
+				                    {safe: true, upsert: false},
+				                    function(err, amodel) {
+							if (err) console.log("Failed to update an action: " + err);
+				                    }
+		                            );
+		                        });
+	                        }
+	                    }
                     }
                     else {  
                         if (err) {
@@ -160,6 +183,7 @@ exports.createAction = function(req, res, next) {
             );
         }
     ); 
+
 };
 
 exports.getActionReport = function(req, res, next) {
@@ -172,3 +196,133 @@ exports.getActionReport = function(req, res, next) {
                         next();
                    });
 }
+
+
+
+
+// simulates the Companies House Extractives API. Dummy controller for not yet existing CH Extractives API data
+exports.getTestdata = function(req, res, next) {
+
+	var test = [{
+		   "ReportDetails"    : {
+		        notes           : "",
+		        reportEndDate   : "2015/10/12",
+		        reportName      : "TestReport1",
+		        companyNumber   : "1",					// country: always GB. from DB: take ID for GB. If new company, add source
+		        companyName     : "Shell",
+		        currency        : "Dollar",
+		        version         : 1,					// check if source for report with current version already exists
+		        referenceNumber : "123",
+		        dateAdded       : "2015/11/11"
+		     }
+		   ,
+		   "projectTotals"           :  [{ projectTotal : {
+		        notes       : "",
+		        amount      : "10,000,000",
+		        projectCode : "abc",
+		        projectName : "secret"			// query DB for projectCode (proj. identifier) first, then project name (might have changed). Deduplication / fuzzy check necessary
+		     }
+		   }],
+		   "governmentPaymentTotals" :  [{ PaymentTotals : {
+		        notes           : "",
+		        amount          : "12,000,000",						// take only payments (not totals). Totals calculated by controller. Check afterwards if sum matches totals
+		        government      : "USA",
+		        countryCodeList : "US",
+		     }
+		   }] ,
+		   "governmentPayments"      :  [{ governmentPayments : {
+		         notes       : "",
+		         amount      : "5,000,000",
+		         government  : "USA",
+		         countryCode : "US",
+		         unitVolume  : {
+		            valuationMethod  : "estimated",
+		            volume           : "10",
+		            unitMeasure      : "Dollar",
+		         }
+		      }
+		    },
+		    { governmentPayments : {
+		         notes       : "",
+		         amount      : "7,000,000",
+		         government  : "USA",
+		         countryCode : "US",
+		         unitVolume  : {
+		            valuationMethod  : "estimated",
+		            volume           : "10",
+		            unitMeasure      : "Dollar",
+		         }
+		      }
+		    }],
+		   "projectPayments"         :  [{ projectPayment : {
+		        notes       : "",
+		        paymentType : "transfer",
+		        amount      : "8,000,000",
+		        projectName : "testproject",		// check if transfer already exists. Source "mandatory...disclosure" must be added to new transfer
+		        projectCode : "tp",
+		        unitVolume  : {
+		            valuationMethod  : "estimated",
+		            volume           : "200",
+		            unitMeasure      : "Dollar",
+		         },
+		        CountryCodeList :"US"
+		     }
+		   }]
+		},
+		{
+			   "ReportDetails"    : {
+			        notes           : "",
+			        reportEndDate   : "2015/07/22",
+			        reportName      : "TestReport2",
+			        companyNumber   : "2",
+			        companyName     : "Exxon",
+			        currency        : "Dollar",
+			        version         : 1,
+			        referenceNumber : "123",
+			        dateAdded       : "2015/08/03"
+			     }
+			   ,
+			   "projectTotals"           :  [{ projectTotal : {
+			        notes       : "",
+			        amount      : "4,000,000",
+			        projectCode : "cde",
+			        projectName : "newproject"
+			     }
+			   }],
+			   "governmentPaymentTotals" :  [{ PaymentTotals : {
+			        notes           : "",
+			        amount          : "30,000,000",
+			        government      : "Germany",
+			        countryCodeList : "DE",
+			     }
+			   }] ,
+			   "governmentPayments"      :  [{ governmentPayments : {
+			         notes       : "",
+			         amount      : "10,000,000",
+			         government  : "Germany",
+			         countryCode : "DE",
+			         unitVolume  : {
+			            valuationMethod  : "estimated",
+			            volume           : "5000",
+			            unitMeasure      : "Euro",
+			         }
+			      }
+			    }],
+			   "projectPayments"         :  [{ projectPayment : {
+			        notes       : "",
+			        paymentType : "Transfer",
+			        amount      : "2,000,000",
+			        projectName : "otherproject",
+			        projectCode : "op",
+			        unitVolume  : {
+			            valuationMethod  : "estimated",
+			            volume           : "2345",
+			            unitMeasure      : "Euro",
+			         },
+			        CountryCodeList :"DE"
+			     }
+			   }]
+			}]
+
+	res.send(test)
+};
