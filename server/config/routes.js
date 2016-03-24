e0711c2c8ae1596e2d7ae8f45ee24483138f0cc1
Mathias Baur <mathias@smartlane.de>
2016-03-24 16:19:36 +0100

    companies house dummy data route added for testing purposes

--- server/config/routes.js
+++ server/config/routes.js
@@ -186,6 +186,14 @@ module.exports	= function(app) {
 	app.put('/api/sources',  sources.updateSource);
 	// DELETE
 	app.delete('/api/sources/:id', sources.deleteSource);
+	
+
+	/////////////////////////////////////////////
+	///// COMPANIES HOUSE DUMMY DATASETS ////////
+	/////////////////////////////////////////////	
+	
+	app.get('/api/testdata', datasets.getTestdata);
+	
 
 	/////////////////////////
 	///// USERS CRUD ////////
