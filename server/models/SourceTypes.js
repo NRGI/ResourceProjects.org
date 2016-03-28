e5757e4fb93b6140fdbc39e3a94a424292834f46
Chris Perry <byndcivilization@gmail.com>
2016-03-27 21:25:17 -0400

    added source type display flag

--- server/models/SourceTypes.js
+++ server/models/SourceTypes.js
@@ -26,6 +26,10 @@ var sourceTypeSchema, SourceType,
 sourceTypeSchema = new Schema({
     source_type_name: String,
     source_type_id: String,
+    source_type_display: {
+        type: boolean,
+        default: false
+    },
     source_type_authority: String,
     source_type_examples: htmlSettings,
     source_type_url_type: String,
@@ -39,9 +43,9 @@ function createDefaultSourceTypes() {
         if(source_type_count === 0) {
             SourceType.create({_id:'56e873691d1d2a3824141427',source_type_name:'Voluntary payment disclosure',source_type_id:'vpd',source_type_authority:'disclosure',source_type_examples:'Voluntary company disclosure - (eg. http://www.kosmosenergy.com/responsibility/transparency.php)',source_type_url_type:'Direct link to data in HTML report'});
             SourceType.create({_id:'56e873691d1d2a3824141429',source_type_name:'Generic Mandatory payment disclosure',source_type_id:'mpd',source_type_authority:'disclosure',source_type_examples:'Mandatory company disclosure (eg. http://www.detnor.no/wp-content/uploads/2015/03/DETNOR-Annual-Report-2014.pdf?d5b1f2 )',source_type_url_type:'Direct link to data in PDF report'});
-            SourceType.create({_id:'56e8736944442a3824141429',source_type_name:'UK Mandatory payment disclosure',source_type_id:'ukdis',source_type_authority:'disclosure',source_type_examples:'Mandatory company disclosure (eg. http://www.detnor.no/wp-content/uploads/2015/03/DETNOR-Annual-Report-2014.pdf?d5b1f2 )',source_type_url_type:'Direct link to data in PDF report'});
-            SourceType.create({_id:'56e8733334442a3824141429',source_type_name:'Norway Mandatory payment disclosure',source_type_id:'nodis',source_type_authority:'disclosure',source_type_examples:'Mandatory company disclosure (eg. http://www.detnor.no/wp-content/uploads/2015/03/DETNOR-Annual-Report-2014.pdf?d5b1f2 )',source_type_url_type:'Direct link to data in PDF report'});
-            SourceType.create({_id:'56e873691d1d2a3824141428',source_type_name:'EITI report',source_type_id:'eiti',source_type_authority:'authoritative',source_type_examples:'EITI report (eg. https://eiti.org/files/EITI-2010-2011-oil-final.pdf)',source_type_url_type:'Direct link to data in XLS report'});
+            SourceType.create({_id:'56e8736944442a3824141429',source_type_name:'UK Mandatory payment disclosure',source_type_display:true,source_type_id:'ukdis',source_type_authority:'disclosure',source_type_examples:'Mandatory company disclosure (eg. http://www.detnor.no/wp-content/uploads/2015/03/DETNOR-Annual-Report-2014.pdf?d5b1f2 )',source_type_url_type:'Direct link to data in PDF report'});
+            SourceType.create({_id:'56e8733334442a3824141429',source_type_name:'Norway Mandatory payment disclosure',source_type_display:true,source_type_id:'nodis',source_type_authority:'disclosure',source_type_examples:'Mandatory company disclosure (eg. http://www.detnor.no/wp-content/uploads/2015/03/DETNOR-Annual-Report-2014.pdf?d5b1f2 )',source_type_url_type:'Direct link to data in PDF report'});
+            SourceType.create({_id:'56e873691d1d2a3824141428',source_type_name:'EITI report',source_type_display:true,source_type_id:'eiti',source_type_authority:'authoritative',source_type_examples:'EITI report (eg. https://eiti.org/files/EITI-2010-2011-oil-final.pdf)',source_type_url_type:'Direct link to data in XLS report'});
             SourceType.create({_id:'56e873691d1d2a382414142e',source_type_name:'Government report',source_type_id:'govreport',source_type_authority:'authoritative',source_type_examples:'Government report (eg. http://www.minfin.gv.ao/docs/dspPetrolDiamond.htm)',source_type_url_type:'Direct link to image or other displaying of information'});
             SourceType.create({_id:'56e873691d1d2a382414142a',source_type_name:'Government database',source_type_id:'govdb',source_type_authority:'authoritative',source_type_examples:'Cadastres, etc.',source_type_url_type:'Information not accessible via direct URL'});
             SourceType.create({_id:'56e873691d1d2a382414142b',source_type_name:'Other authoritative source',source_type_id:'otherauth',source_type_authority:'authoritative',source_type_examples:'Other source',source_type_url_type:'Other'});
