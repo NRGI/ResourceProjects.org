f9351a15dff5603a2fd59d1f3fee21b1d04bc159
Chris Perry <byndcivilization@gmail.com>
2016-03-31 01:37:07 -0400

    country pages working

--- server/controllers/countries.js
+++ server/controllers/countries.js
@@ -339,9 +339,9 @@ exports.getCountryByID = function(req, res) {
                             });
                         }
                         if (site.site_commodity.length>0) {
-                            if (_.where(country.company_commodity, {_id:_.last(site.site_commodity)._id}).length<1) {
-                                country.company_commodity.push({
-                                    _id: _.last(site.site_commodity)._id,
+                            if (_.where(country.commodities, {_id: _.last(site.site_commodity).commodity._id}).length<1) {
+                                country.commodities.push({
+                                    _id: _.last(site.site_commodity).commodity._id,
                                     commodity_name: _.last(site.site_commodity).commodity.commodity_name,
                                     commodity_type: _.last(site.site_commodity).commodity.commodity_type,
                                     commodity_id: _.last(site.site_commodity).commodity.commodity_id
@@ -793,7 +793,6 @@ exports.getCountryByID = function(req, res) {
             res.send(country);
         }
     }
-
 };
 
 exports.createCountry = function(req, res, next) {
