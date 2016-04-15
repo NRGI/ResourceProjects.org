a281fb0fbcf6b067535bb061aa0b2371bf86ba47
Chris Perry <byndcivilization@gmail.com>
2016-04-15 13:52:25 -0400

    SAVEPOINT

--- server/config/express.js
+++ server/config/express.js
@@ -13,6 +13,7 @@ module.exports = function(app, config) {
 	function compile(str, path) {
 		return stylus(str).set('filename', path);
 	}
+    console.log(config);
 	// set up view engine
 	app.set('views', config.rootPath + '/server/views');
 	app.set('view engine', 'jade');
