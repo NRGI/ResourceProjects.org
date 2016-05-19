e2144286420a6bca973d421343c17b7cb9035611
Chris Perry <byndcivilization@gmail.com>
2016-05-19 18:08:22 -0400

    added infinite scroll on companies page

--- public/app/companies/nrgiCompanyListCtrl.js
+++ public/app/companies/nrgiCompanyListCtrl.js
@@ -3,50 +3,36 @@
 angular.module('app')
     .controller('nrgiCompanyListCtrl', function (
         $scope,
+        $rootScope,
         nrgiAuthSrvc,
         nrgiIdentitySrvc,
         nrgiCompaniesSrvc
     ) {
-        $scope.limit = 50;
-        $scope.page = 0;
+        var limit = 50,
+            currentPage = 0,
+            totalPages = 0,
+            _ = $rootScope._;
+
         $scope.count =0;
-        $scope.show_count=0;
-        var loadCompanies = function(limit,page){
-            nrgiCompaniesSrvc.query({skip: page, limit: limit}, function (success) {
-                $scope.count = success.count;
-                $scope.limit = limit;
-                $scope.page = page;
-                $scope.companies=success.data;
-                $scope.show_count = success.data.length+$scope.page;
-                $scope.record_type = 'companies';
-            });
-        };
+        $scope.busy = false;
 
-        loadCompanies($scope.limit,$scope.page);
+        nrgiCompaniesSrvc.query({skip: currentPage*limit, limit: limit}, function (response) {
+            $scope.count = response.count;
+            $scope.companies = response.data;
+            totalPages = Math.ceil(response.count / limit);
+            currentPage = currentPage + 1;
+        });
 
-        $scope.select = function(changeLimit){
-            loadCompanies(changeLimit,0);
-        };
-
-        $scope.next = function(page,count){
-            loadCompanies($scope.limit,count);
-        };
-
-        $scope.prev = function(page){
-            loadCompanies($scope.limit,page-$scope.limit);
-        };
-
-        $scope.first = function(){
-            loadCompanies($scope.limit,0);
-        };
-
-        $scope.last = function(page){
-            if($scope.count%$scope.limit!=0){
-                page =  parseInt($scope.count/$scope.limit)*$scope.limit;
-                loadCompanies($scope.limit,page);
-            }else {
-                loadCompanies($scope.limit,page-$scope.limit);
+        $scope.loadMore = function() {
+            if ($scope.busy) return;
+            $scope.busy = true;
+            if(currentPage < totalPages) {
+                nrgiCompaniesSrvc.query({skip: currentPage*limit, limit: limit}, function (response) {
+                    $scope.companies = _.union($scope.companies, response.data);
+                    currentPage = currentPage + 1;
+                    $scope.busy = false;
+                });
             }
-        }
+        };
     });
 
