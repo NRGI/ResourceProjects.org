bde9b805912cf1452dd9d408027ac97e11449ea9
Chris Perry <byndcivilization@gmail.com>
2016-06-01 16:15:16 -0400

    SAVEPOINT

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ var config 	= require('./server/config/config')[env];
 var user = process.env.DB_ID;
 var pass = process.env.DB_KEY;
 
-// require('./server/config/express')(app, config);
-
 require('./server/config/express')(app, config, user, pass, env);
 
 require('./server/config/passport')();
