6e77f4cb0692b5bc86ef992e37fcafcd18511d5c
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2016-06-13 12:46:31 +0300

    Add download for list pages #118

--- public/app/dynamic/companies/nrgiCompanyListCtrl.js
+++ public/app/dynamic/companies/nrgiCompanyListCtrl.js
@@ -16,11 +16,20 @@ angular.module('app')
         $scope.count =0;
         $scope.busy = false;
 
+        var company_group_name, str;
+        var com = ', ';
+        $scope.csv_companies = [];
+        var fields = ['company_name', 'company_groups', 'project_count', 'site_count', 'field_count', 'transfer_count'];
+        var header_companies = ['Company', 'Group(s)', 'Projects', 'Sites', 'Fields', 'Payments'];
+        $scope.getHeaderCompanies = function () {
+            return header_companies
+        };
         nrgiCompaniesSrvc.query({skip: currentPage*limit, limit: limit}, function (response) {
             $scope.count = response.count;
             $scope.companies = response.data;
             totalPages = Math.ceil(response.count / limit);
             currentPage = currentPage + 1;
+            $scope.createDownloadList($scope.companies);
         });
 
         $scope.loadMore = function() {
@@ -31,8 +40,39 @@ angular.module('app')
                     $scope.companies = _.union($scope.companies, response.data);
                     currentPage = currentPage + 1;
                     $scope.busy = false;
+                    $scope.createDownloadList($scope.companies);
                 });
             }
+        };
+        $scope.createDownloadList = function (companies) {
+            angular.forEach(companies, function (company, key) {
+                $scope.csv_companies[key] = [];
+                angular.forEach(fields, function (field) {
+                    if (field == 'company_groups') {
+                        if(company[field]!=undefined&&company[field].length > 0) {
+                            str = '';
+                            angular.forEach(company[field], function (group, i) {
+                                company_group_name = '';
+                                if (group != undefined) {
+                                    company_group_name = group.company_group_name.toString();
+                                    company_group_name = company_group_name.charAt(0).toUpperCase() + company_group_name.substr(1);
+                                }
+                                if (i != company[field].length - 1 && company_group_name != '') {
+                                    str = str + company_group_name + com;
+                                } else {
+                                    str = str + company_group_name;
+                                    $scope.csv_companies[key].push(str);
+                                }
+                            });
+                        } else {
+                            $scope.csv_companies[key].push('')
+                        }
+                    }
+                    if (field != 'company_groups') {
+                        $scope.csv_companies[key].push(company[field])
+                    }
+                })
+            });
         };
     });
 
