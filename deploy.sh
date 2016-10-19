992b1ea9c266ab12c7686c761aa5c8b4b8f8a034
Ionut Prunache <ionut@vitaminsoftware.com>
2016-10-19 12:31:55 +0300

    #208 Add deploy trigger script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Triggers automatic deploy for certain branches.
+#
+# Uses Shippable env variables:
+# - BRANCH
+# - COMMIT
+
+elementIn () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+# Automatic deploy allowed for these branches only.
+DEPLOY_BRANCHES=("staging" "feature-208-trigger-deploy")
+
+if ! elementIn "$BRANCH" "${DEPLOY_BRANCHES[@]}" ;
+then
+  echo "Skiping deploy as branch is not allowed for automatic deploy"
+fi
+
+# Id of the Shippable project containing the deploy script.
+DEPLOY_PROJECT_ID=5804f143e8fe021000f9aed1
+
+# Trigger Shippable to run the deploy project and pass the current project name, branch and latest commit
+curl\
+  -H "Authorization: apiToken $API_TOKEN"\
+  -H "Content-Type: application/json"\
+  -d "{\"branchName\":\"master\",\"globalEnv\": {\"PROJECT\":\"rp\", \"PROJECT_BRANCH\":\"$BRANCH\", \"PROJECT_COMMIT\":\"$COMMIT\"}}"\
+  "https://api.shippable.com/projects/$DEPLOY_PROJECT_ID/newBuild"
