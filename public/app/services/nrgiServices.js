8c8df37b9df5ee520944a4a3b0b7e7f8f91ed65d
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2016-10-31 15:52:47 +0200

    savepoint

--- public/app/services/nrgiServices.js
+++ public/app/services/nrgiServices.js
@@ -120,7 +120,7 @@ angular.module('app')
         return SourceResource;
     })
     .factory('nrgiSourceTypesSrvc', function($resource) {
-        var SourceTypeResource = $resource('/api/sourcetypes/:limit/:skip/:display/:_id', {_id: "@id", limit: "@limit", skip: "@skip", display: "@display"}, {
+        var SourceTypeResource = $resource('/api/sourcetypes/:limit/:skip/:_id', {_id: "@id", limit: "@limit", skip: "@skip"}, {
             query:  {method:'GET', isArray: false},
             update: {method: 'PUT', isArray: false}
         });
