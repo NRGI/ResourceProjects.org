9e5cb036a5860b58b9c020c596d8462f9b2807e2
Ionut Prunache <ionut@vitaminsoftware.com>
2016-11-09 12:03:58 +0200

    #230 Enable continuous deployment for the master branch

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ elementIn () {
 }
 
 # Automatic deploy allowed for these branches only.
-DEPLOY_BRANCHES=("staging" "feature-208-trigger-deploy")
+DEPLOY_BRANCHES=("staging" "master")
 
 if ! elementIn "$BRANCH" "${DEPLOY_BRANCHES[@]}" ;
 then
