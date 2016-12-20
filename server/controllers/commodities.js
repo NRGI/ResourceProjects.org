9a577bdcb41709288325158a682509c3b6e2012e
KseniyaKuprina <light.it.zp+kseniya@gmail.com>
2016-12-20 17:07:01 +0200

    delete comnsole.log

--- server/controllers/commodities.js
+++ server/controllers/commodities.js
@@ -42,7 +42,6 @@ exports.getCommodities = function(req, res) {
                 data.errorList = errors.errorFunction(err, 'Commodity not found');
                 res.send(data);
             } else {
-                console.log(qq)
                 data.count = commodity_count;
                 callback(null, data);
             }
